09dbbe6ed6ccd31d930071a288ce7980be4ec476
Slava Kirey <dizel@mail.ua>
2016-03-02 10:28:47 +0200

    IC-1 fix menu

--- js/default.js
+++ js/default.js
@@ -1,4 +1,9 @@
 /**
  * Created by slavakirey on 2/23/16.
  */
-
+//$(document).ready(function(){
+//    $('.js-menu').on('mouseover', function(){
+//        $(this).find(".nav__menu").fadeIn();
+//        console.log('jashv');
+//    })
+//});
